d3292326fc050ef1c195bba5b8bee684318e2631
Abdurrahim  KURT <48498001+abdurrahimkurt@users.noreply.github.com>
2021-09-24 01:14:27 +0300

    design changes

--- lib/main.dart
+++ lib/main.dart
@@ -44,53 +44,110 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Container(height: 50,width: 140,
-          child: Image.asset("assets/images/myNoteanaekran.png",fit: BoxFit.fill,)),
-        centerTitle: true,
-      ),
-      body: Container(
-        color: Colors.black87,
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Henüz gösterilecek hiçbir görevin bulunmamaktadır.',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
+    Size size = MediaQuery.of(context).size;
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title:
+              /* Container(
+              height: 50,
+              width: 40,
+              child: Image.asset(
+                "assets/images/myNotelogos.png",
+                fit: BoxFit.fill,
+              )), */
+              TabBar(
+            labelColor: Colors.red,
+            indicatorColor: Colors.grey,
+            unselectedLabelColor: Colors.grey,
+            tabs: [
+              Tab(
+                text: "Bugün",
               ),
-              Text(
-                "Hemen yeni bir görev oluştur",
-                style: Theme.of(context).textTheme.headline5,
+              Tab(
+                text: "Haftalık",
+              ),
+              Tab(
+                text: "Aylık",
               ),
             ],
           ),
+          centerTitle: true,
+        ),
+        body: Container(
+          color: Colors.black87,
+          child: Center(
+            child: SingleChildScrollView(
+              physics: AlwaysScrollableScrollPhysics(),
+              scrollDirection: Axis.vertical,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  /* Text(
+                    'Henüz gösterilecek hiçbir görevin bulunmamaktadır.',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "Hemen yeni bir görev oluştur",
+                    style: Theme.of(context).textTheme.headline5,
+                  ), */
+                  Container(
+                    height: size.height * 0.8,
+                    child:
+                        TabBarView(children: [gunluk(), haftalik(), aylik()]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.red,
+          onPressed: () {
+            Navigator.pushNamed(context, "/add_task");
+          },
+          child: Icon(Icons.add),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          backgroundColor: Colors.black,
+          unselectedItemColor: Colors.white,
+          fixedColor: Colors.white,
+          type: BottomNavigationBarType.fixed,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.notes_outlined), label: "Görevlerim"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.list_alt_outlined), label: "Geçmiş Görevler")
+            /*  BottomNavigationBarItem(icon: Icon(Icons.today_outlined), label: "Bugün"),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_view_week_outlined), label: "Haftalık"),
+            BottomNavigationBarItem(icon: Icon(Icons.task_rounded), label: "Aylık"), */
+          ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: Colors.red,
-        onPressed: () {
-          Navigator.pushNamed(context, "/add_task");
-        },
-        child: Icon(Icons.add),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        items: [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.notes_outlined), label: "Görevlerim"),
-          BottomNavigationBarItem(icon: Icon(Icons.today_outlined), label: "Bugün"),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.calendar_view_week_outlined), label: "Haftalık"),
-          BottomNavigationBarItem(icon: Icon(Icons.task_rounded), label: "Aylık"),
-        ],
-      ),
+    );
+  }
+
+  Widget gunluk() {
+    return Container(
+      color: Colors.black87,
+    );
+  }
+
+  Widget haftalik() {
+    return Container(
+      color: Colors.yellow,
+    );
+  }
+
+  Widget aylik() {
+    return Container(
+      color: Colors.blue,
     );
   }
 }
--- lib/screens/add_task.dart
+++ lib/screens/add_task.dart
@@ -2,6 +2,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'package:getwidget/components/button/gf_button.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:mynote/services/firebase_crud.dart';
 import 'package:mynote/widgets/rounded_input_field.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -22,121 +24,167 @@ class _AddTaskState extends State<AddTask> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text("Görev Ekle"),
+        centerTitle: true,
       ),
-      body: SingleChildScrollView(
+      body: Container(
+        /* height: size.height,
+        width: size.width, */
+        color: Colors.black,
         child: Center(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 18, 8, 8),
-                child: RoundedInputField(
-                    icon: FontAwesomeIcons.edit,
-                    key: null,
-                    hintText: "Görev Adını Giriniz..",
-                    onChanged: (value) {
-                      setState(() {
-                        gorevAdi = value;
-                      });
-                    }),
-              ),
-              RoundedInputField(
-                  icon: FontAwesomeIcons.edit,
-                  key: null,
-                  hintText: "Görev Açıklamasını Giriniz..",
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  child: Column(
+                    children: [
+                      RoundedInputField(
+                          icon: FontAwesomeIcons.edit,
+                          key: null,
+                          hintText: "Görev Adını Giriniz..",
+                          onChanged: (value) {
+                            setState(() {
+                              gorevAdi = value;
+                            });
+                          }),
+                      RoundedInputField(
+                          icon: FontAwesomeIcons.edit,
+                          key: null,
+                          hintText: "Görev Açıklamasını Giriniz..",
+                          onChanged: (value) {
+                            setState(() {
+                              gorevAciklamasi = value;
+                            });
+                          }),
+                    ],
+                  ),
+                ),
+
+                /* TimePickerWidget(
+                  hintText: "Tarih Seçiniz",
                   onChanged: (value) {
-                    setState(() {
-                      gorevAciklamasi = value;
-                    });
-                  }),
-              /* TimePickerWidget(
-                hintText: "Tarih Seçiniz",
-                onChanged: (value) {
-                  gorevTarihi = value;
-                },
-              ), */
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                      onPressed: () {
-                        DatePicker.showDatePicker(context,
-                            showTitleActions: true,
-                            minTime: DateTime(2018, 3, 5),
-                            maxTime: DateTime(2019, 6, 7),
-                            theme: DatePickerTheme(
-                                headerColor: Colors.orange,
-                                backgroundColor: Colors.blue,
-                                itemStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 18),
-                                doneStyle: TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 16)), onChanged: (date) {
-                          print('change $date in time zone ' +
-                              date.timeZoneOffset.inHours.toString());
-                        }, onConfirm: (date) {
-                          gorevTarihi =
-                              date.toUtc().toString().substring(0, 10);
-                          print('confirm $gorevTarihi aa');
-                        }, currentTime: DateTime.now(), locale: LocaleType.en);
-                      },
-                      child: Row(
-                        children: [
-                          Icon(Icons.calendar_today_rounded),
-                          Text("Tarih Seç"),
-                        ],
-                      )),
-                  ElevatedButton(
-                      onPressed: () {
-                        DatePicker.showTimePicker(context,
-                            showTitleActions: true, onChanged: (date) {
-                          print('change $date in time zone ' +
-                              date.timeZoneOffset.inHours.toString());
-                        }, onConfirm: (date) {
-                          gorevSaati = date.toString().substring(11, 16);
-                          print('confirm $gorevSaati');
-                        }, currentTime: DateTime.now());
-                      },
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Icon(Icons.watch_later_sharp),
-                          Text("Saat Seç"),
-                        ],
-                      )),
-                ],
-              ),
-              ElevatedButton(
-                  onPressed: () {
-                    // ignore: unnecessary_null_comparison
-                    if (gorevSaati == null) {
-                      print("Görevin bitiş tarihini giriniz");
-                      // ignore: unnecessary_null_comparison
-                    } else if (gorevTarihi == null) {
-                      print("Görevin bitiş saatini giriniz");
-                    } else {
-                      Map<String, dynamic> gorevBilgileri = {
-                        'gorevAdi': gorevAdi,
-                        'gorevAciklamasi': gorevAciklamasi,
-                        'gorevTarihi': gorevTarihi,
-                        'gorevSaati': gorevSaati,
-                      };
-                      crud.addData(gorevBilgileri);
-                      print(gorevAdi +
-                          "   " +
-                          gorevAciklamasi +
-                          "    " +
-                          gorevTarihi);
-                      print("Görevin bitiş saatini giriniz");
-                    }
+                    gorevTarihi = value;
                   },
-                  child: Text("Görevi oluştur"))
-            ],
+                ), */
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      GFButton(
+                        onPressed: () {
+                          DatePicker.showDatePicker(context,
+                              showTitleActions: true,
+                              minTime: DateTime(2018, 3, 5),
+                              maxTime: DateTime(2019, 6, 7),
+                              theme: DatePickerTheme(
+                                  headerColor: Colors.orange,
+                                  backgroundColor: Colors.blue,
+                                  itemStyle: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18),
+                                  doneStyle: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 16)), onChanged: (date) {
+                            print('change $date in time zone ' +
+                                date.timeZoneOffset.inHours.toString());
+                          }, onConfirm: (date) {
+                            gorevTarihi =
+                                date.toUtc().toString().substring(0, 10);
+                            print('confirm $gorevTarihi aa');
+                          },
+                              currentTime: DateTime.now(),
+                              locale: LocaleType.en);
+                        },
+                        text: "Tarih seç",
+                        icon: Icon(Icons.calendar_today_rounded,
+                            color: Colors.white),
+                        shape: GFButtonShape.pills,
+                        size: GFSize.LARGE,
+                        color: Colors.grey.shade800,
+                      ),
+                      /* ElevatedButton(
+                          onPressed: () {
+                            DatePicker.showTimePicker(context,
+                                showTitleActions: true, onChanged: (date) {
+                              print('change $date in time zone ' +
+                                  date.timeZoneOffset.inHours.toString());
+                            }, onConfirm: (date) {
+                              gorevSaati = date.toString().substring(11, 16);
+                              print('confirm $gorevSaati');
+                            }, currentTime: DateTime.now());
+                          },
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Icon(Icons.watch_later_sharp),
+                              Text("Saat Seç"),
+                            ],
+                          ),), */
+                      GFButton(
+                        onPressed: () {
+                          DatePicker.showTimePicker(context,
+                              showTitleActions: true, onChanged: (date) {
+                            print('change $date in time zone ' +
+                                date.timeZoneOffset.inHours.toString());
+                          }, onConfirm: (date) {
+                            gorevSaati = date.toString().substring(11, 16);
+                            print('confirm $gorevSaati');
+                          }, currentTime: DateTime.now());
+                        },
+                        text: "Saat seç",
+                        icon:
+                            Icon(Icons.watch_later_sharp, color: Colors.white),
+                        shape: GFButtonShape.pills,
+                        size: GFSize.LARGE,
+                        color: Colors.grey.shade800,
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(top: 50),
+                  height: size.height * 0.07,
+                  width: size.width * 0.4,
+                  child: GFButton(
+                    onPressed: () {
+                      if (gorevAdi == "") {
+                        print("Görevin bitiş tarihini giriniz");
+                      } else if (gorevAciklamasi == "") {
+                        print("Görevin bitiş saatini giriniz");
+                      } else {
+                        Map<String, dynamic> gorevBilgileri = {
+                          'gorevAdi': gorevAdi,
+                          'gorevAciklamasi': gorevAciklamasi,
+                          'gorevTarihi': gorevTarihi,
+                          'gorevSaati': gorevSaati,
+                        };
+                        crud.addData(gorevBilgileri);
+                        print(gorevAdi +
+                            "   " +
+                            gorevAciklamasi +
+                            "    " +
+                            gorevTarihi);
+                        print("Görevi oluştur");
+                      }
+                    },
+                    text: "Görev Oluştur",
+                    icon: Icon(Icons.watch_later_sharp, color: Colors.white),
+                    shape: GFButtonShape.pills,
+                    size: GFSize.LARGE,
+                    color: Colors.grey.shade800,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
